ff66bd43d10fccc14703b32c0711ac3b546a8557
eneax <19818937+eneax@users.noreply.github.com>
2018-07-08 21:57:54 +0200

    Setup dumb header

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <header className="bg-gold sans-serif tc">
+      <div className="mw9 center pa4 pt5-ns ph7-l">
+        <h1 className="f2 f1-m f-subheadline-l measure-narrow lh-title mv0">
+          <span className="bg-black-90 lh-copy white pa1 tracked-tight">
+            Al Centro del Mondo
+          </span>
+        </h1>
+        <p className="measure center f5 f4-m lh-copy">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+          tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+          vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+          no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+    </header>
   )
 }
 
