5630296e192f883fe06e43dd292c87203deb8d48
eneax <19818937+eneax@users.noreply.github.com>
2018-07-10 18:31:40 +0200

    Add Canada to tours.js

--- src/utils/tours.js
+++ src/utils/tours.js
@@ -732,5 +732,75 @@ export default [
     name: 'Il Meglio del Canada',
     days: '16 giorni',
     price: 'A partire da € 2495.00 a persona',
+    imgOne: 'https://s3-us-west-2.amazonaws.com/prnt/hw-080411-cargo_960.jpg',
+    imgTwo: 'https://s3-us-west-2.amazonaws.com/prnt/hw-080411-cargo_960.jpg',
+    imgThree: 'https://s3-us-west-2.amazonaws.com/prnt/hw-080411-cargo_960.jpg',
+    imgFour: 'https://s3-us-west-2.amazonaws.com/prnt/hw-080411-cargo_960.jpg',
+    resources: [
+      {
+        day: '1° GIORNO - MONTREAL',
+        desc: "Arrivo all'aeroporto di Montreal, trasferimento in albergo. Pernottamento."
+      },
+      {
+        day: '2° GIORNO - MONTREAL',
+        desc: "Prima colazione in hotel. In mattinata visita della città vecchia. Giornata a disposizione per attività individuali. Pernottamento."
+      },
+      {
+        day: '3° GIORNO - MONTREAL * LAC DELAGE',
+        desc: "Prima colazione in hotel. Partenza per i monti Appalachi, dove si potrà ammirare la produzione dello sciroppo d'acero. Pranzo tipico. Nel pomeriggio arrivo a Lac Delage e tempo a disposizione per usufruire dei servizi del resort. Cena in hotel e pernottamento."
+      },
+      {
+        day: '4° GIORNO - LAC DELAGE * SAGUENAY',
+        desc: "Prima colazione in hotel. Giornata ricca di attività nella riserva dei Monti Laurenziani, con guide naturaliste. Si parte su uno scuolabus per la prima escursione nella foresta. Gita in canoa nella foresta boreale. Pranzo sulla riva del lago con l’esplorazione delle dighe dei castori. Dopo il tramonto avvistamento degli orsi bruni. La giornata termina a Saguenay. Pernottamento."
+      },
+      {
+        day: '5° GIORNO - SAGUENAY * QUEBEC CITY',
+        desc: "Prima colazione in hotel. Partenza lungo il fiordo di Saguenay per Tadoussac. Imbarco per l’escursione all'avvistamento delle balene. Si prosegue per la regione del Charlevoix, dichiarata dall'Unesco Riserva Mondiale della Biosfera. Pranzo al famoso Manoir Richelieu. Partenza per la costa di Beauprè per ammirare le cascate di Montmorency. Arrivo a Quebec e pernottamento."
+      },
+      {
+        day: '6° GIORNO - QUEBEC CITY * OTTAWA',
+        desc: 'Prima colazione in hotel. Visita di Quebec City, dove si potranno ammirare la Place Royale, i Piani di Abramo, la Parliament Hill e il Chateau Frontenac. Partenza per St. Prosper e visita della fattoria dove si trova un allevamento di bisonti. Tipico pranzo a base di bisonte. Proseguimento per Ottawa. Sistemazione e pernottamento.'
+      },
+      {
+        day: '7° GIORNO - OTTAWA * TORONTO',
+        desc: "Prima colazione in hotel. In mattinata visita della National Gallery of Canada. Al termine proseguimento per la regione delle Mille Isole. Crociera nel 'Giardino del Grande Spirito'. Arrivo a Toronto e visita del centro con Bay Street, il distretto finanziario, la City Hall, il Parlamento dell'Ontario, l'Università di Toronto, Yonge Street e l'Eaton Center. Sistemazione in hotel e pernottamento."
+      },
+      {
+        day: '8° GIORNO - ESCURSIONE A NIAGARA FALLS',
+        desc: 'Partenza per le Cascate. Pranzo in ristorante panoramico. Nel pomeriggio mini-crociera ai piedi delle cascate sul battello Hornblower. Seguendo la Niagara Parkway si arriva a Niagara on the Lake, una graziosa cittadina. Rientro a Toronto e pernottamento.'
+      },
+      {
+        day: '9° GIORNO - TORONTO * CALGARY - IN VOLO',
+        desc: 'Trasferimento in aeroporto e volo per Calgary. Arrivo a Calgary, trasferimento in hotel. Pernottamento.'
+      },
+      {
+        day: '10° GIORNO - CALGARY * BANFF NATIONAL PARK',
+        desc: 'Visita della città di Calgary.  Plaza e la Calgary Tower. Partenza per il Parco Nazionale di Banff. Pranzo in stile barbecue. Si prosegue verso il Lake Louise, il Moraine Lake e le famose cime "Wenchemntka". Arrivo nel pomeriggio a Banff, visita della cittadina con Tunnel Mountain e le Cascate Bow. Sistemazione in hotel e pernottamento.'
+      },
+      {
+        day: '11° GIORNO - BANFF * JASPER NATIONAL PARK * BANFF',
+        desc: 'Partenza per il Parco Nazionale di Jasper, la Icefields Parkway. Pranzo lungo il percorso. Ai Columbia Icefields escursione a bordo di un "gatto delle nevi" sul ghiacciaio Athabasca. Ritorno a Banff facendo sosta ai laghi Bow e Peyto. Pernottamento a Banff.'
+      },
+      {
+        day: '12° GIORNO - BANFF * SHUSWAP LAKE',
+        desc: 'Dopo la prima colazione si percorre la Trans-Canada highway verso il British Columbia. Proseguimento verso la Roger Pass e le grandi vette delle Selkirk Mountains. Nel pomeriggio escursione nella foresta pluviale del Revelstoke National Park.'
+      },
+      {
+        day: '13° GIORNO - SHUSWAP LAKE * VICTORIA + TRAGHETTO',
+        desc: "Si viaggia lungo la Coaquihalla highway per l'imbarco sul traghetto B.C. Ferries per l’Isola di Vancouver; la traversata dello Stretto di Georgia fino a Swartz Bay. Proseguimento per Victoria e visita della capitale del British Columbia; si vedrà il parco di Beacon Hill, Chinatown, Tolmie Mountain, il quartiere di Uplands, Marine Drive; ed è inclusa la visita ai Butchart Gardens. Sistemazione in hotel e pernottamento."
+      },
+      {
+        day: '14° GIORNO - VICTORIA * VANCOUVER + TRAGHETTO',
+        desc: 'Trasferimento a Swartz Bay per una traversata del Georgia Straight e delle Gulf Islands fino a Tsawwassen e proseguimento per Vancouver. Pranzo in un ristorante del centro. Nel pomeriggio visita di Vancouver Point. Sistemazione in hotel e pernottamento.'
+      },
+      {
+        day: '15° GIORNO – VANCOUVER * Italia',
+        desc: 'Prima colazione in hotel. Rilascio della camera in mattinata e trasferimento in aeroporto per il rientro in Italia.'
+      },
+      {
+        day: '16° GIORNO – Italia',
+        desc: 'Arrivo in Italia.'
+      }
+    ]
   },
 ];
