8d4551841767b65b84f2039779850062f8e23269
eneax <19818937+eneax@users.noreply.github.com>
2018-07-10 21:16:12 +0200

    Update logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.jpg';
 
 const Navbar = () => {
   return (
     <nav className="db dt-l w-100 border-box pa3 ph5-l">
       <Link to='/' className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" title="Home">
-        <img src="http://tachyons.io/img/logo.jpg" className="dib w2 h2 br-100" alt="Logo" />
+        <img src={logo} className="dib w2 h2 br-100" alt="Logo" />
       </Link>
 
       <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
