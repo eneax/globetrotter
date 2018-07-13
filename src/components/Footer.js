c3271f80bacaefd593f6b67f48a32dafe27c9036
eneax <19818937+eneax@users.noreply.github.com>
2018-07-13 16:42:10 +0200

    Update Footer links color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="pv4 ph3 ph5-m ph6-l mid-gray">
-      <small className="f6 db tc">© 2018 AL CENTRO DEL MONDO s.r.l. Unipersonale</small>
+      <small className="f7 db tc">© 2018 AL CENTRO DEL MONDO s.r.l. Unipersonale</small>
       <div className="tc mt3">
-        <a className="f6 dib ph2 link mid-gray dim" href="mailto:info@alcentrodelmondo.it" target="_blank" rel="noopener noreferrer" title="Al Centro del Mondo Email">
+        <a className="f7 dib ph2 link dim mail" href="mailto:info@alcentrodelmondo.it" target="_blank" rel="noopener noreferrer" title="Al Centro del Mondo Email">
           <i className="fas fa-envelope fa-2x"></i>
         </a>
-        <a className="f6 dib ph2 link mid-gray dim" href="https://www.facebook.com/Al-Centro-del-Mondo-Viaggi-e-Turismo-113138121866/" target="_blank" rel="noopener noreferrer" title="Al Centro del Mondo on Facebook">
+        <a className="f7 dib ph2 link dim facebook" href="https://www.facebook.com/Al-Centro-del-Mondo-Viaggi-e-Turismo-113138121866/" target="_blank" rel="noopener noreferrer" title="Al Centro del Mondo on Facebook">
           <i className="fab fa-facebook-f fa-2x"></i>
         </a>
-        <a className="f6 dib ph2 link mid-gray dim" href="https://twitter.com/AlCentrodelMond" target="_blank" rel="noopener noreferrer" title="Al Centro del Mondo on Twitter">
+        <a className="f7 dib ph2 link dim twitter" href="https://twitter.com/AlCentrodelMond" target="_blank" rel="noopener noreferrer" title="Al Centro del Mondo on Twitter">
           <i className="fab fa-twitter fa-2x"></i>
         </a>
       </div>
