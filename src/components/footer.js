ab1e8eb1a5d084b3cf5abd2730ff94c008cfab2e
eneax <19818937+eneax@users.noreply.github.com>
2019-01-26 10:10:58 +0100

    Refactor footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import { SocialLink } from '../elements'
 import { above, avenir, darkGrey } from '../utils'
 
